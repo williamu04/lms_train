61615fe733cc8baa3e3627a8e3615ad08ae3bb47
williamu04 <williamputra04@gmai.com>
2025-03-21 11:15:38 +0700

    add sicebar and nav

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+
+export const Logo = () => {
+    return (
+        <Image
+            height={130}
+            width={130}
+            src="/logo.svg"
+            alt="Logo"
+        />
+    )
+}
